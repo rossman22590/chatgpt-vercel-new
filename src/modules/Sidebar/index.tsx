831fdf4ae0322a09ccfb69cd0307c8f288c6e84c
Ross Cohen <rosscohen1990@gmail.com>
2023-05-26 01:50:41 -0400

    Update index.tsx

--- src/modules/Sidebar/index.tsx
+++ src/modules/Sidebar/index.tsx
@@ -83,7 +83,7 @@ const Sidebar: FC<{
       <div className="p-4 mt-2 flex items-baseline justify-between">
         <span className="text-3xl text-gradient font-[800]">PixGen+</span>
         <a
-          href="#"
+          href="https://myapps.ai"
           target="_blank"
           rel="noreferrer"
         >
